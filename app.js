b09a167366aad7a285a53d58cc7ad90325d923b8
Ed Summers <ehs@pobox.com>
2022-10-17 18:29:28 -0400

    space

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 import { WARCParser } from 'https://unpkg.com/warcio/dist/warcio.js';
-import {LitElement, html, css} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
+import { LitElement, html, css } from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
 
 class WARCAnalyzer extends LitElement {
 
